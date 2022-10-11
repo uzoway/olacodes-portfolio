8cbe147f53a3be8d4fc86f705b5fb1e3ad14a7ff
uzoway <victor14944.ov@gmail.com>
2022-10-11 21:48:10 +0100

    chore:Configured the projects content, color and font family in the tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js}"
+  ],
   theme: {
-    extend: {},
+    colors: {
+      'pale': '#d9d9d9',
+      'white': '#ffffff'
+    },
+    fontFamily: {
+      'spline-sans':  ['Spline sans', 'sans-serif'],
+      'bebas': ['Bebas Neue', 'cursive']
+    }
   },
   plugins: [],
 }
