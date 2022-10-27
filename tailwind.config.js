f213c4e650b5b216d2e5498d4d48ba14d6180820
uzoway <victor14944.ov@gmail.com>
2022-10-27 19:22:41 +0100

    fix: Add the 'index.html' content to tell tailwind that the index file can be in the root directory.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}"
+    "./src/**/*.{html,js}",
+    "./index.html"
   ],
   theme: {
     colors: {
